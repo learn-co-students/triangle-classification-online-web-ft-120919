8cef05de12c944317c81a004a566f8018d71c611
github username <github email address>
2019-12-24 08:27:53 -0800

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
+require 'pry'
 class Triangle
-  # write code here
+
+  attr_reader :l_1, :l_2, :l_3 
+
+  def initialize(l_1, l_2, l_3)
+    @l_1 = l_1
+    @l_2 = l_2
+    @l_3 = l_3
+    
+
+  end
+  
+  def kind
+    # binding.pry
+    if l_1 > 0 && l_2 > 0 && l_3 > 0
+      if l_1 == l_2 && l_2 == l_3
+        :equilateral
+      elsif l_1 == l_2 && l_2 != l_3 || l_2 == l_3 && l_2 != l_1 || l_1 == l_3 && l_1 != l_2
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  
+  end
+
+  class TriangleError < StandardError
+    'TriangleError'
+  end
+
 end
+
+
