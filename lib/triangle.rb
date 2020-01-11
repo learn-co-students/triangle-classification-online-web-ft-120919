a43d303820e05cc79d5184be5c8f839629754acf
mrarthurwhite <mister.arthur.white@gmail.com>
2020-01-11 01:59:40 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  attr_accessor :s1, :s2, :s3
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+# triangle inequality conditions
+    s1s2= (@s1+@s2)>@s3
+    s2s3= (@s2+@s3)>@s1
+    s1s3= (@s1+@s3)>@s2
+# raise error if the inequality is broken
+    if (!s1s2 || !s2s3 || !s1s3)
+      raise TriangleError
+    end
+
+
+    if @s1==@s2 && @s2==@s3
+      return :equilateral
+    elsif  @s1==@s2 || @s2==@s3 || @s1==@s3
+      return :isosceles
+    else
+      return :scalene
+    end
+
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
+
 end
