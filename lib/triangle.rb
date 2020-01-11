38aec3678926a0a07af22e400b98e8785dddf101
Devon James Wright <devonjamess.wright@gmail.com>
2020-01-11 18:19:24 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :side_1,:side_2,:side_3
+  def initialize(side_1,side_2,side_3)
+   @side_1 = side_1
+   @side_2 = side_2
+   @side_3 = side_3
+  end 
+  
+  def kind
+    if (@side_1 + @side_2) <= @side_3 or (@side_2 + @side_3) <= @side_1 or (@side_1 + @side_3) <= @side_2 or @side_1.abs != @side_1 or @side_2.abs != @side_2 or @side_3.abs != @side_3 or @side_1 == 0 or @side_2 == 0 or @side_3 == 0 
+      raise TriangleError
+    elsif @side_1 == @side_2 && @side_3 == @side_1 && @side_1 > 0 
+      :equilateral
+    elsif @side_1 == @side_2 && @side_1 != @side_3 && @side_1 > 0 && @side_3 > 0 && (@side_1 + @side_2) > @side_3 or @side_1 == @side_3 && @side_1 != @side_2 && @side_1 > 0 && @side_2 > 0 && (@side_1 + @side_2) > @side_3 or @side_2 == @side_3 && @side_2 != @side_1 && @side_2 > 0 && @side_1 > 0 && (@side_2 + @side_3) > @side_1
+      :isosceles
+    elsif @side_1 != @side_2 && @side_1 != @side_3 && @side_3 != @side_2 && @side_1 > 0 && @side_2 > 0 && @side_3 > 0
+      :scalene 
+    end 
+  end 
+  
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
